058f374af8821cc2c26fa4e17a1e520af5697a8b
Kyle <775207318@qq.com>
2019-09-02 14:49:49 +0800

    完成轮播图的制作

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    <el-carousel :interval="5000" arrow="always">
+  <div class="container">
+    <el-carousel height="700px" arrow="always">
       <el-carousel-item v-for="(item,index) in banners" :key="index">
-        <div class="banners-image" :style="`background:url(${$axios.defaults.baseURL}${item.url})`">
-
-        </div>
+        <div
+          class="bannerImage"
+          :style="`background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat;background-size:contain contain;`"
+        ></div>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -12,42 +13,39 @@
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      banners:[]
-    }
+      banners: []
+    };
   },
-  mounted(){
+  mounted() {
     this.$axios({
-      url:'/scenics/banners'
+      url: "/scenics/banners"
     })
-    .then((res)=>{
-    console.log(res)
-    // if(res.status === 200){
-      this.banners = res.data.data
-    // }
-    })
-    .catch((err)=>{
-    console.log(err)
-    })
+      .then(res => {
+        console.log(res);
+        // if(res.status === 200){
+        this.banners = res.data.data;
+        // }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 };
 </script>
 
 <style scoped lang="less">
- .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
-  
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
+.bannerImage {
+  width: 1000px;
+  height: 700px;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
 </style>
